a43e6e89fa6cd37785485b1a19fc4ee22609f227
Sanyalak T. <isanyalakt@gmail.com>
2025-03-20 15:03:12 +0700

    update aboutme

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import AboutMe from "./components/AboutMe"
+
 export default function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <div className="h-screen flex justify-center bg-blue-950">
+      <div className="p-6 gap-y-6 flex flex-col justify-start w-[80%] lg:w-[70%]">
+        <h1 className="w-full p-6 bg-amber-100 font-extrabold text-blue-500">
+          React App Starter
+        </h1>
+        <section className="w-full p-5 bg-amber-100 flex">
+          <ul className="list-inside list-disc flex-1">
+            <span className="font-semibold text-orange-400">Tech Stack:</span>
+            <li>Vite</li>
+            <li>React</li>
+            <li>JavaScript</li>
+            <li>Tailwind</li>
+          </ul>
+        </section>
+        <hr />
+        <AboutMe name="Sanyalak"/>
+      </div>
+    </div>
     </>
   )
 }
